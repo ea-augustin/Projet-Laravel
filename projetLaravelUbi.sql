6889b85c67eab52ebba7ff83b07cc65080408039
Benjamin Bonsignore <benjaminbonsignore@outlook.fr>
2019-02-26 09:46:04 +0100

    SQL+env

---
+++ projetLaravelUbi.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Mar 26 Février 2019 à 09:33
+-- Version du serveur :  5.7.25-0ubuntu0.18.04.2
+-- Version de PHP :  7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projetLaravelUbi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Achat`
+--
+
+CREATE TABLE `Achat` (
+  `id` int(55) NOT NULL,
+  `user_id` int(55) NOT NULL,
+  `product_id` int(55) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Panier`
+--
+
+CREATE TABLE `Panier` (
+  `id` int(55) NOT NULL,
+  `user_id` int(55) NOT NULL,
+  `product_id` int(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Product`
+--
+
+CREATE TABLE `Product` (
+  `id` int(55) NOT NULL,
+  `label` text NOT NULL,
+  `price` int(55) NOT NULL,
+  `type` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(55) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `Achat`
+--
+ALTER TABLE `Achat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id_2` (`user_id`);
+
+--
+-- Index pour la table `Panier`
+--
+ALTER TABLE `Panier`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Index pour la table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Achat`
+--
+ALTER TABLE `Achat`
+  ADD CONSTRAINT `Achat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`);
+
+--
+-- Contraintes pour la table `Panier`
+--
+ALTER TABLE `Panier`
+  ADD CONSTRAINT `Panier_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+
+--
+-- Contraintes pour la table `Product`
+--
+ALTER TABLE `Product`
+  ADD CONSTRAINT `Product_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Achat` (`product_id`);
+
+--
+-- Contraintes pour la table `Users`
+--
+ALTER TABLE `Users`
+  ADD CONSTRAINT `Users_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Panier` (`user_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
