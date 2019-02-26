411e35c6f5dd2cd5c902915157399f222a2a8144
Ezra Augustin <elidoom88@gmail.com>
2019-02-26 11:16:09 +0100

    second commit

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,8 +2,8 @@
 
 @section ('content')
    <h1>Users</h1>
-   @if(count($users)>0)
-   @foreach($users as $user)
+   @if(count($Users)>0)
+   @foreach($Users as $User)
    <div class="card card-body bg-light">
    <h3><a href="/newProjet/laravel2/public/users/{{$user->id}}">{{$user->name}}</a></h3></div>
  
